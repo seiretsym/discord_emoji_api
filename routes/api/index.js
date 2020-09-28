b15a446886c4f8417b11ae1c15ae56c02dffa833
Kerwin Hy <kerwin.hy@gmail.com>
2020-09-28 11:21:41 -0700

    1. add express package 2. fix routing issue in routes/api/index.js 3. add .gitignore

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 // route to specific apis
-router.use("/")
+router.route("/")
   .get((req, res) => {
     console.log(req.query);
     res.json(req.query);
